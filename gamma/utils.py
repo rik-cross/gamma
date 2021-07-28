d25a7a37f7db87f7a62bdebfbd76798f6a2c38e2
Rik Cross <rik@raspberrypi.org>
2021-07-28 15:41:56 +0100

    Added function to draw a box (non-filled)

--- gamma/utils.py
+++ gamma/utils.py
@@ -5,3 +5,13 @@ def drawRect(s,x,y,w,h,c,a=255):
     overlay.set_alpha(a)
     overlay.fill(c)
     s.blit(overlay, (x,y))
+
+def drawBox(s,x,y,w,h,c):
+    # top
+    pygame.draw.line(s,c,(x,y),(x+w,y))
+    # bottom
+    pygame.draw.line(s,c,(x,y+h),(x+w,y+h))
+    # left
+    pygame.draw.line(s,c,(x,y),(x,y+h))
+    # right
+    pygame.draw.line(s,c,(x+w,y),(x+w,y+h))
