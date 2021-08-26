0bf96c90ee66c1bdf12bc0e888c6478bb8323d05
Rik Cross <rik@raspberrypi.org>
2021-08-26 19:55:47 +0100

    Sound manager improvements

--- gamma/manager_sound.py
+++ gamma/manager_sound.py
@@ -6,6 +6,7 @@ class SoundManager:
         self.soundVolume = 1.0
         self.musicVolume = 1.0
         self.targetMusicVolume = 1.0
+        self.volumeIncrement = 0.01
         self.nextMusic = None
         self.currentMusic = None
         self.sounds = {}
@@ -23,28 +24,31 @@ class SoundManager:
         # don't play the music if already playing
         if musicName is self.currentMusic:
             return
-        
         pygame.mixer.music.load(self.music[musicName])
-        pygame.mixer.music.set_volume(self.musicVolume)
         self.currentMusic = musicName
         pygame.mixer.music.play(-1)
-    def playMusicFade(self, musicName):
-
+    def playMusicFade(self, musicName, duration=500):
         # don't play the music if already playing
         if musicName is self.currentMusic:
             return
-
         # add music to queue
         self.nextMusic = musicName
         # fade out current music
-        self.fadeOut()
-    def fadeOut(self):
-        pygame.mixer.music.fadeout(500)
+        self.fadeOut(duration)
+    def setMusicVolume(self, volume, duration=1):
+        self.volumeIncrement = 1/duration
+        self.targetMusicVolume = volume
+    def fadeOut(self, duration):
+        pygame.mixer.music.fadeout(duration)
     def update(self):
         # raise volume if lower than target
         if self.musicVolume < self.targetMusicVolume:
-            self.musicVolume = min(self.musicVolume + 0.005, self.targetMusicVolume)
+            self.musicVolume = min(self.musicVolume + self.volumeIncrement, self.targetMusicVolume)
             pygame.mixer.music.set_volume(self.musicVolume)
+        # lower volume if higher than target
+        if self.musicVolume > self.targetMusicVolume:
+            self.musicVolume = max(self.musicVolume - self.volumeIncrement, self.targetMusicVolume)
+            pygame.mixer.music.set_volume(self.musicVolume)        
         # play next music if appropriate
         if self.nextMusic is not None:
             # if 'old' music has finished fading out
